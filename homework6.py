a81a409f2931997b1a07e43c3c189dcd92ed9a63
pavel.romanowski <33510707+subzik@users.noreply.github.com>
2019-06-26 13:10:30 +0300

    homework6

---
+++ homework6.py
@@ -0,0 +1,20 @@
+from jinja2 import Template
+
+template = Template('Hello {{ name }}!')
+message = template.render(name='John Doe')
+
+print(message)  # 'Hello John Doe!'
+import yaml
+from jinja2 import Template
+
+with open('data.yml') as data_file:
+    config_data = yaml.load(data_file)
+
+with open('vhosts.j2') as template_file:
+    template_html = template_file.read()
+
+template = Template(template_html)
+vhosts_conf = template.render(config_data)
+
+with open('vhosts.conf', 'w') as vhosts_file:
+     vhosts_file.write(vhosts_conf)
