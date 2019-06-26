0f15706d1ed14189a5c8862eb379796a23cacb35
pavel.romanowski <33510707+subzik@users.noreply.github.com>
2019-06-27 02:42:07 +0300

    hometask4.v2 with fixes

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-DevOps Lab 2019 (May-August)
+# ps-stats
+### The application for finding repo statistics on GitHub
+
+---
+For starting, please run ps-stats from directory, where file located.
+
+Running the application with options:
+
+**python pr-stats --repo `<num_repo>`**
+
+`-h | --help`  - take a help
+
+`--repo` - repo's number is mandatory option
+
+
+ `-v | --version`   - version
+ 
+`-u` - Last user, who opened repo number
+ 
+`-o` - day of the week opened
+
+`-ho` hours of the day opened
+
+For opening repositories on browser, **[click](https://github.com/alenaPy/devops_lab/pulls)**. 
+
+
+```html
+version 1.0
+```
+
---
+++ pr-stats.py
@@ -0,0 +1,49 @@
+import requests
+import datetime
+import calendar
+import argparse
+
+version = "1.0"
+user = "subzik"
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-u", "--user", help="user", action="store_true")
+parser.add_argument("--repo", required=True, help="insert number of github pull-request")
+parser.add_argument("-v", "--version", help="version script", version="ver. 1.0", action="version")
+parser.add_argument("-o", "--open", help="Info about time open pull", action="store_true")
+parser.add_argument("-d", "--dayofweek", help="Day of the week opened", action="store_true")
+parser.add_argument("-ho", "--hours", help="Hours of the day opened", action="store_true")
+parser.add_argument("--token", type=str, default="dde16a3421cf097e1565dbc2c6a3a5b5c684a533")
+
+
+args = parser.parse_args()
+
+if args.repo:
+    pull_num = args.repo
+else:
+    pull_num = 17
+
+
+url = 'https://api.github.com/repos/alenaPy/devops_lab/pulls/%s' % pull_num
+pulls = requests.get(url, auth=(user, args.token))
+pullsjson = pulls.json()
+jsondate = pullsjson["created_at"]
+converted_created_at = datetime.datetime.strptime(jsondate, "%Y-%m-%dT%H:%M:%SZ")
+d_opened = datetime.datetime.now() - converted_created_at
+d_of_week = calendar.day_name[converted_created_at.weekday()]
+h_ofday = converted_created_at.hour
+u_opened = pullsjson['user']['login']
+
+
+if args.open:
+
+    print("Counts opened: ", str(d_opened.days))
+if args.dayofweek:
+    print("Day of the week opened: ", d_of_week)
+if args.hours:
+    print("Hours of the day opened: ", str(h_ofday))
+if args.user:
+    if args.repo is not None:
+        print("Last user, who opened repo number", args.repo, "is:",  u_opened)
+    else:
+        pull_num = input('Please type repo\'s number: ')
