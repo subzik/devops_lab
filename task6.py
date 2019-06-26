a2cd229524ec6e4b7868f5d0b0689b46e942d7c5
pavel.romanowski <33510707+subzik@users.noreply.github.com>
2019-06-26 13:12:15 +0300

    Delete task6.py

--- task6.py
+++
@@ -1,7 +0,0 @@
-# task5
-Students, Count = map(int, input(('Please, insert count of student and subject: ')).split())
-marks = []
-for subject in range(Count):
-    marks += [map(float, input().split())]
-for scores in zip(*marks):
-    print(sum(scores) / Count)
