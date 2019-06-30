4a553bfb95149ca72bdf102069ceae8fb5d4119a
pavel.romanowski <33510707+subzik@users.noreply.github.com>
2019-06-30 10:35:53 +0300

    test fixed

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 
-print("press ENTER & insert word 'amsterdam' or 'alaska'")
+#print("press ENTER & insert word 'amsterdam' or 'alaska'")
 
 
-def word1(n):
+def word1(words):
     r1 = ('q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p')
     r2 = ('a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l')
     r3 = ('z', 'x', 'c', 'b', 'n', 'm')
 
-    words = str(input("please insert the word: "))
-    words = set(words)
-    if words.intersection(r1) and words.intersection(r2)\
-            or words.intersection(r1) and words.intersection(r3):
+    #words = str(input("please, insert the word: "))
+    b = str(words)
+
+    n = set(words)
+    if n.intersection(r1) and n.intersection(r2) or n.intersection(r1) and n.intersection(r3):
         return False
-    elif words.intersection(r2) and words.intersection(r3):
+    elif n.intersection(r2) and n.intersection(r3):
         return False
     else:
         return True
 
 
 if __name__ == "__main__":
-    w1 = str(input())
-    print(word1(w1))
+    words = input("Input word: ")
+    print(word1(words))
+
--- test_app.py
+++ test_app.py
@@ -9,7 +9,9 @@ class UnitTest(TestCase):
 
     def test_task5(self):
         self.assertFalse(app.word1("amsterdam"))
-        self.assertTrue(app.word("alaska"))
+        self.assertTrue(app.word1("alaska"))
+        self.assertTrue(app.word1("portu"))
+        self.assertFalse(app.word1("rome"))
 
     def tearDown(self):
         """Finish"""
