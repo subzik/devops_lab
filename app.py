fe299c239503ba970a9475695010764223e84a44
pavel.romanowski <33510707+subzik@users.noreply.github.com>
2019-06-30 10:43:48 +0300

    flake8 fixed

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 
-#print("press ENTER & insert word 'amsterdam' or 'alaska'")
-
 
 def word1(words):
     r1 = ('q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p')
     r2 = ('a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l')
     r3 = ('z', 'x', 'c', 'b', 'n', 'm')
-
-    #words = str(input("please, insert the word: "))
-    b = str(words)
 
     n = set(words)
     if n.intersection(r1) and n.intersection(r2) or n.intersection(r1) and n.intersection(r3):
@@ -22,4 +17,3 @@ def word1(words):
 if __name__ == "__main__":
     words = input("Input word: ")
     print(word1(words))
-
